271d5104fe1c28fbce4c1b9cf0847ada8a7869f5
Sancho Tech <munashebmachisi@gmail.com>
2025-06-30 04:44:49 +0200

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'Jarvis AI',
+  author: '‎',
+  botName: "Jarvis AI-V1.0",
+  botOwner: 'Sancho Tech', // Your name
+  ownerNumber: '263715659766', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is bot was developed by Sancho Tech for managing group commands and automating tasks.",
+  version: "2.0.1",
+};
+
+module.exports = settings;
